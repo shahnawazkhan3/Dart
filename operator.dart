85f13f73857d0d560bd74b7a610f7ae2a3dda59e
shahnawazGold <shahnawazkhan_333@yahoo.com>
2018-04-02 18:14:57 +0500

    1st 2/4/2018

---
+++ operator.dart
@@ -0,0 +1,16 @@
+
+
+void main (){
+  
+  print ("yes");
+  
+  var num1= 2;
+  var num2=4;
+  var res = 0;
+  res=num1+num2;
+  print("addition : ${res}");
+  res = num1-num2;
+  print("sub : ${res}");
+  
+
+}
